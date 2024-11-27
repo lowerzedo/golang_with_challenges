35ba51930059b4ba5aa61398c28bbb8e2b9bd72c
Nazar Hasanov <tp052475@mail.apu.edu.my>
2024-11-27 12:41:49 +0800

    feat: Challenge 2

---
+++ challenge2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func createRange(start, end int) []int {
+	var result []int
+	for i := start; i <= end; i++ {
+		result = append(result, i)
+	}
+	return result
+}
+
+func generatePattern(n int) string {
+	if n > 10 {
+		fmt.Println("The number should be less than 10")
+	}
+
+	var numbers []int
+	var temp string
+	var delim string = ","
+
+	numbers = createRange(1, n)
+
+	for len(numbers) > 0 {
+		temp = temp + strings.Trim(strings.Replace(fmt.Sprint(numbers), " ", delim, -1), "[]") + "\n"
+		numbers = numbers[:len(numbers)-1]
+	}
+
+	fmt.Println(temp)
+
+	return temp
+}
+
+func main() {
+	generatePattern(10)
+}
